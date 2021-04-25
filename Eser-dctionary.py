c53b5b3fbfb3b65bd75d0aeba9ce220d9b11f18d
Andrea1512 <74495948+Andrea1512@users.noreply.github.com>
2021-04-26 00:26:38 +0200

    Add files via upload

---
+++ Eser-dctionary.py
@@ -0,0 +1,37 @@
+nome_studente1 = str(input("Inserisci nome 1° studente: "))
+cognome_studente1 = str(input("Inserisci cognome 1° studente: "))
+email1 = str(input("Inserisci indirizzo email 1° studente: ")) 
+voto_mate_studente1 = int(input("Inserisci voto matematica 1° studente))
+
+nome_studente2 = str(input("Inserisci nome 2° studente: "))
+cognome_studente2 = str(input("Inserisci cognome 2° studente: "))
+email2 = str(input("Inserisci indirizzo email 2° studente: ")) 
+voto_mate_studente2 = int(input("Inserisci voto matematica 2° studente))
+
+nome_studente3 = str(input("Inserisci nome 3° studente: "))
+cognome_studente3 = str(input("Inserisci cognome 3° studente: "))
+email3 = str(input("Inserisci indirizzo email 3° studente: ")) 
+voto_mate_studente3 = int(input("Inserisci voto matematica 3° studente))
+
+nome_studente4 = str(input("Inserisci nome 4° studente: "))
+cognome_studente4 = str(input("Inserisci cognome 4° studente: "))
+email4 = str(input("Inserisci indirizzo email 4 °studente: ")) 
+voto_mate_studente4 = int(input("Inserisci voto matematica 4° studente))
+
+nome_studente5 = str(input("Inserisci nome 5° studente: "))
+cognome_studente5 = str(input("Inserisci cognome 5° studente: "))
+email5 = str(input("Inserisci indirizzo email 5 °studente: ")) 
+voto_mate_studente5 = int(input("Inserisci voto matematica 5° studente))
+
+email = input("Inserisci mail studente: ")
+
+if email == email1:
+   print(studente1)
+elif email == email2:
+   print(studente2)
+elif email == email3:
+   print(studente3)
+elif email == email4:
+   print(studente4)
+elif email == email5:
+   print(studente5)              
