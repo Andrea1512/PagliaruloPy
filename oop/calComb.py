e54bf2f0864dde8655a68e6694ce00a471060e65
Andrea1512 <74495948+Andrea1512@users.noreply.github.com>
2021-10-13 01:07:56 +0200

    Update calComb.py

--- oop/calComb.py
+++ oop/calComb.py
@@ -1,1 +1,15 @@
-calComb
+class calcComb():
+
+    def __init__(self, n1, n2):
+
+        self.__N = len(stringa)
+        self.__stringa = n1
+        self.__listStringa = list(n1)
+
+    def get_stringa(self.__n2):
+        return self.__stringa
+
+    def get_listStringa(self.__n1):
+        return self.__listStringa
+
+    def setStringa(self):
